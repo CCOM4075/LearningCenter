49f64678623223bb3f7ef05f08436e870eaab982
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2020-11-14 15:38:13 -0400

    Revert "Update connection.php"
    This reverts commit b820b5488453ee03176ca30ddc1b1ca7fcf5c1e4.

--- includes/connection.php
+++ includes/connection.php
@@ -5,6 +5,4 @@
     $basededatos = "learning_center_flg";
     $connection = mysqli_connect($servidor, $usuario, "", $basededatos)
     or die ("No se ha podido conectar al servidor de Base de datos");	
-
-    session_start();
 ?>
