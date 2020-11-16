fbb52530bfbbbc1db2a395db5a3f308d1576e645
Leonardo Sotomayor <leonardo.sotomayor@upr.edu>
2020-11-16 17:23:10 -0400

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,8 +1,8 @@
 <?php
     $usuario = "root";
-    $contrasena = "";
+    $password = "";
     $servidor = "localhost";
     $basededatos = "learning_center_flg";
-    $connection = mysqli_connect($servidor, $usuario, "", $basededatos)
+    $connection = mysqli_connect($servidor, $usuario, $password, $basededatos)
     or die ("No se ha podido conectar al servidor de Base de datos");	
 ?>
