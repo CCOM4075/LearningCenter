0ce4305e9faeede0b35be0dbfafa8a4f520741eb
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2020-11-16 22:31:43 -0400

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,10 +3,10 @@
 <div class="row">
     <div class="col-md-12">
         <div class="copyright">
-        <!--
+        
             <p>Copyright © 2020. Todos los derechos reservados. Plantilla por <a href="https://colorlib.com">Colorlib</a>.</p>
             <p>No animals were harmed on the development of this page : )</p>
-        -->
+       
         </div>
     </div>
 </div>
