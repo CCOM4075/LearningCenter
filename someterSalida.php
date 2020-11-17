ee74e0a1a931fabed6e07d0b23025a9fc482efff
Leonardo Sotomayor <leonardo.sotomayor@upr.edu>
2020-11-16 21:15:31 -0400

    Create someterSalida.php

---
+++ someterSalida.php
@@ -0,0 +1,27 @@
+<?php
+    if(empty($_REQUEST['asistenciaID']))
+    {
+        header("location: index.php");
+    }else{
+        include './includes/connection.php';
+        include_once './functions/functions.php';
+        $asistenciaID = $_REQUEST['asistenciaID'];
+
+        $query = mysqli_query($connection,"SELECT horaDeSalida FROM asistencia WHERE id = '$asistenciaID'");
+        $horaSalida = mysqli_fetch_array($query);
+
+        if($horaSalida['horaDeSalida'] == '00:00:00')
+        {
+           $horaActual = getCurrentHour();
+            $queryHoraSalida = mysqli_query($connection,"UPDATE asistencia SET horaDeSalida = '$horaActual' WHERE id = '$asistenciaID'");
+            header("location: index.php"); 
+        }
+        else
+            header("location: index.php"); 
+        
+
+
+
+    }
+
+?>
