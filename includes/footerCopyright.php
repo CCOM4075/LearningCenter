2effb1952aee62077a2e4eba1d2c29d2dda03464
Leonardo Sotomayor <leonardo.sotomayor@upr.edu>
2020-11-16 22:31:13 -0400

    Create footerCopyright.php

---
+++ includes/footerCopyright.php
@@ -0,0 +1,8 @@
+<!-- copyright -->
+<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2020 ClickPhone. All rights reserved
+			</p>
+		</div>
+	</div>
+<!-- //copyright -->
