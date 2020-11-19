4694be58f4edd162390d922465e114471c9edbcc
Leonardo Sotomayor <leonardo.sotomayor@upr.edu>
2020-11-18 21:54:24 -0400

    Update practica.php

--- practica.php
+++ practica.php
@@ -3,8 +3,12 @@
 <body>
 
 <?php
-$hoy = new DateTime();
-echo $hoy;
+include './includes/connection.php';
+include './functions/functions.php';
+    echo todayHojaAsistenciaExist();
+    $hojaAsistencia = getUltimaHojaAsistencia();
+    echo $hojaAsistencia;
+    //$query = mysqli_query($connection, "INSERT INTO asistencia(participanteID, proposito, horaDeEntrada, hojaAsistencia) values('10', '1', ', '$hojaAsistencia')");
 ?> 
 
 </body>
