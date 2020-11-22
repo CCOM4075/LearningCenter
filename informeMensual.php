d0133ea29470ce8f0ce50d623bb8caf21f32a8d5
Leonardo Sotomayor <leonardo.sotomayor@upr.edu>
2020-11-22 16:42:32 -0400

    Update informeMensual.php

--- informeMensual.php
+++ informeMensual.php
@@ -3,6 +3,7 @@
     include './includes/connection.php';
     include_once './functions/functions.php';    
     verificarBibliotecario($_SESSION['rol_usuario']);
+    
     if(!empty($_POST))
 	{
         $mes = $_POST['mes'];
