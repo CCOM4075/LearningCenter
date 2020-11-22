b1ff02a11c3faf9238810f2928a9f15c8843b483
Leonardo Sotomayor <leonardo.sotomayor@upr.edu>
2020-11-22 16:01:58 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,8 @@
-<?php
-    /*session_start();
-	include './includes/connection.php';
-
-	$id = $_SESSION['id_usuario'];		
-	$query = mysqli_query($connection, "SELECT * FROM usuarios WHERE userID = '$id'");
-			
-	$data = mysqli_fetch_array($query);*/
-
-?>
-
 <!--Header Descktop-->
-
+<?php
+    $name = getUserName($_SESSION['id_usuario']);
+    $userName = getUserUsuario($_SESSION['id_usuario']);   
+?>
 <header class="header-desktop">
     <div class="section__content section__content--p30">
         <div class="container-fluid">
@@ -32,23 +24,23 @@
                     <div class="account-wrap">
                         <div class="account-item clearfix js-item-menu">
                             <div class="image">
-                                <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                <img src="images/icon/avatar-01.jpg"/>
                             </div>
                             <div class="content">
-                                <a class="js-acc-btn" href="#">Juan Del Town</a>
+                                <a class="js-acc-btn" href="#"><?php if(!empty($_SESSION['active']))echo $name;?></a>
                             </div>
                             <div class="account-dropdown js-dropdown">
                                 <div class="info clearfix">
                                     <div class="image">
                                         <a href="#">
-                                            <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                            <img src="images/icon/avatar-01.jpg"/>
                                         </a>
                                     </div>
                                     <div class="content">
                                         <h5 class="name">
-                                            <a href="#">Juan Del Town</a>
+                                            <a href="#"><?php if(!empty($_SESSION['active']))echo $name;?></a>
                                         </h5>
-                                        <span class="email">juandeltown@example.com</span>
+                                        <span class="email"><?php if(!empty($_SESSION['active']))echo $userName;?></span>
                                     </div>
                                 </div>
                                 <div class="account-dropdown__body">
